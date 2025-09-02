4f51fb9eb45ee662b490a9d1ccbcf762307de040
Vasyl Kovalov <raid3r81@gmail.com>
2025-09-02 21:00:42 +0300

    Добавлен класс UploadedFileStorage для работы с файлами
    В коде реализован класс `UploadedFileStorage`, который включает методы `SaveFileAsync` для асинхронного сохранения загруженных файлов с уникальными именами и `DeleteFile` для удаления файлов по имени. Файлы сохраняются в директорию `wwwroot/uploads`.

---
+++ FeedbackFormRazor/Models/Services/UploadedFileStorage.cs
@@ -0,0 +1,39 @@
+﻿namespace FeedbackFormRazor.Models.Services;
+
+public class UploadedFileStorage
+{
+    public async Task<string> SaveFileAsync(IFormFile file)
+    {
+        var uploadPath = Path.Combine("wwwroot", "uploads");
+
+        if (file == null || file.Length == 0)
+        {
+            throw new ArgumentException("File is null or empty", nameof(file));
+        }
+        // Ensure the upload directory exists
+        if (!Directory.Exists(uploadPath))
+        {
+            Directory.CreateDirectory(uploadPath);
+        }
+        // Generate a unique filename to avoid overwriting existing files
+        var uniqueFileName = $"{Guid.NewGuid()}_{Path.GetFileName(file.FileName)}";
+        var filePath = Path.Combine(uploadPath, uniqueFileName);
+        // Save the file to the specified path
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+        return filePath; // uniqueFileName; // Return the unique filename for reference
+    }
+
+    public void DeleteFile(string fileName)
+    {
+        var uploadPath = Path.Combine("wwwroot", "uploads");
+        var filePath = Path.Combine(uploadPath, fileName);
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+    }
+
+}
