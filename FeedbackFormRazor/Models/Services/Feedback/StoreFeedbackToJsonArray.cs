c701d43e4bda105630f54d909dfa5a8494ea4493
Vasyl Kovalov <raid3r81@gmail.com>
2025-09-02 21:00:29 +0300

    Изменения в классе StoreFeedbackToJsonArray
    Обновлен конструктор для внедрения зависимостей, добавлены параметры `FeedbackRepository` и `UploadedFileStorage`. Удалены прямые операции с файлом `feedbacks.json`, теперь используется метод `AddAsync` для добавления отзывов и `SaveFileAsync` для сохранения загруженных файлов. Добавлены комментарии на русском языке для улучшения понимания кода.

--- FeedbackFormRazor/Models/Services/Feedback/StoreFeedbackToJsonArray.cs
+++ FeedbackFormRazor/Models/Services/Feedback/StoreFeedbackToJsonArray.cs
@@ -3,46 +3,23 @@ using System.Text.Json;
 
 namespace FeedbackFormRazor.Models.Services.Feedback;
 
-public class StoreFeedbackToJsonArray : IFeedbackSender
+public class StoreFeedbackToJsonArray(
+    FeedbackRepository feedbackRepository,
+    UploadedFileStorage uplodedFileStorage
+    ) : IFeedbackSender
 {
     public async Task SendFeedbackAsync(FeedbackForm feedbackForm)
     {
-        var uploadsDir = Path.Combine("wwwroot", "uploads");
-
-        var storageFileDir = Path.Combine("storage");
-        if (!Directory.Exists(storageFileDir))
-        {
-            Directory.CreateDirectory(storageFileDir);
-        }
-        var storageFilePath = Path.Combine(storageFileDir, "feedbacks.json");
-
-        // get feedbacks array from file 
-        if (!File.Exists(storageFilePath))
-        {
-            await File.WriteAllTextAsync(storageFilePath, "[]");
-        }
-        using var reader = new StreamReader(storageFilePath);
-        var fileContent = await reader.ReadToEndAsync();
-        var feedbacks = string.IsNullOrEmpty(fileContent)
-            ? new List<FeedbackModel>()
-            : System.Text.Json.JsonSerializer.Deserialize<List<FeedbackModel>>(fileContent);
-        reader.Close();
-        reader.Dispose();
-
+        // Збереження файлу який переданий від клієнта
         var imagePath = string.Empty;
-        var nextId = feedbacks.Any() ? feedbacks.Max(f => f.Id) + 1 : 1;
         if (feedbackForm.Image != null)
         {
-            var imageExt = Path.GetExtension(feedbackForm.Image.FileName).ToLower();
-            var newImageName = $"image_{DateTime.Now:yyyyMMdd_HHmmss}{imageExt}";
-            imagePath = Path.Combine(uploadsDir, newImageName);
-            using var fileStream = System.IO.File.Create(imagePath);
-            await feedbackForm.Image.CopyToAsync(fileStream);
+            imagePath = await uplodedFileStorage.SaveFileAsync(feedbackForm.Image);
         }
 
-        feedbacks.Add(new FeedbackModel
+        // Збереження даних форми
+        await feedbackRepository.AddAsync(new FeedbackModel
         {
-            Id = nextId,
             Name = feedbackForm.Name,
             Gender = feedbackForm.Gender,
             Birthday = feedbackForm.Birthday,
@@ -52,12 +29,5 @@ public class StoreFeedbackToJsonArray : IFeedbackSender
             Favorites = feedbackForm.Favorites,
             ImageFile = imagePath,
         });
-
-        var newFileContent = JsonSerializer.Serialize(feedbacks,
-            new System.Text.Json.JsonSerializerOptions
-            {
-                WriteIndented = true
-            });
-        await File.WriteAllTextAsync(storageFilePath, newFileContent);
     }
 }
